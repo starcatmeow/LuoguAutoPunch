1d02c64fa9f3b0eda4fa364798107d14a5fb602d
Dongruixuan Li <dongruixuan@hotmail.com>
2020-10-26 12:48:33 +0800

    Luogu Auto Punch v1.0

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,18 @@
-FROM python:3.8-alpine3.10
+FROM python:3.8-buster
 
 WORKDIR /app
-# update apk repo
-RUN echo "http://dl-4.alpinelinux.org/alpine/v3.10/main" >> /etc/apk/repositories && \
-    echo "http://dl-4.alpinelinux.org/alpine/v3.10/community" >> /etc/apk/repositories
 
-# install chromedriver
-RUN apk update && apk add chromium chromium-chromedriver
-
-COPY . /app
-
-# install requirements
-RUN pip install --upgrade pip && pip uninstall numpy && pip install -r requirements.txt
+RUN wget -q https://dl-ssl.google.com/linux/linux_signing_key.pub && apt-key add linux_signing_key.pub && rm linux_signing_key.pub
+RUN sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+RUN apt-get -y update
+RUN apt-get install -y google-chrome-stable
+RUN apt-get install -yqq unzip
+RUN wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`/chromedriver_linux64.zip
+RUN unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
+ENV DISPLAY=:99
+ENV TF_CPP_MIN_LOG_LEVEL=2
+COPY requirements.txt /app
+RUN pip install --upgrade pip && pip install -r requirements.txt && rm requirements.txt
+COPY app.py /app
 
 ENTRYPOINT [ "python", "app.py" ]
--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 from PIL import Image
+import tensorflow as tf
 import os
-import muggle_ocr
 import time
 import re
 
@@ -13,7 +14,7 @@ try:
     LUOGU_USERNAME = os.environ['LUOGU_USERNAME']
     LUOGU_PASSWORD = os.environ['LUOGU_PASSWORD']
 except:
-    print('请在环境变量中设置 LUOGU_USERNAME, LUOGU_PASSWORD ！')
+    print('请在环境变量中设置 LUOGU_USERNAME, LUOGU_PASSWORD ！', flush=True)
     exit(4)
 
 def login(driver):
@@ -30,20 +31,17 @@ def login(driver):
     right = captcha.location['x'] + captcha.size['width']
     bottom = captcha.location['y'] + captcha.size['height']
 
-    left=left*2
-    top=top*2
-    right=right*2
-    bottom=bottom*2
-
     im = Image.open('captcha.png') 
     im = im.crop((left, top, right, bottom))
     im.save('captcha.png')
 
     with open(r"captcha.png","rb") as f:
         captcha_bytes = f.read()
-    sdk = muggle_ocr.SDK(model_type=muggle_ocr.ModelType.Captcha)
+    with tf.device('/CPU:0'):
+        import muggle_ocr
+        sdk = muggle_ocr.SDK(model_type=muggle_ocr.ModelType.Captcha)
     captcha_text = sdk.predict(image_bytes=captcha_bytes)
-    print("验证码: "+captcha_text)
+    print("验证码: "+captcha_text, flush=True)
 
     time.sleep(1)
     inputs[2].send_keys(captcha_text)
@@ -53,10 +51,10 @@ def login(driver):
     
 def check(driver):
     if driver.current_url != LUOGU_LOGIN_URL:
-        print('登录成功')
+        print('登录成功', flush=True)
         return True
     else:
-        print('登录失败')
+        print('登录失败', flush=True)
         return False
 
 def punch(driver):
@@ -65,10 +63,10 @@ def punch(driver):
     try:
         punch = driver.find_element_by_link_text("点击打卡")
         driver.execute_script("arguments[0].click();", punch)
-        print('打卡成功')
+        print('打卡成功', flush=True)
         return True
     except:
-        print('已经打过卡了')
+        print('已经打过卡了', flush=True)
         return False
 
 def get_result(driver):
@@ -77,23 +75,26 @@ def get_result(driver):
         result_origin = driver.find_element_by_xpath("//span[contains(@class,'lg-punch-result')]").text
         result = re.match(r'§ (.*) §',result_origin).group(1)
         day = driver.find_element_by_xpath("//div[contains(@class,'am-u-sm')]//strong").text
-        print("已连续打卡 {} 天，今日运势：{}".format(day,result))
+        print("已连续打卡 {} 天，今日运势：{}".format(day,result), flush=True)
         return True
     except:
-        print("无法获取打卡结果")
+        print("无法获取打卡结果", flush=True)
         return False
-        
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
-driver = webdriver.Chrome()
+
+chrome_options = Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--headless')
+driver = webdriver.Chrome(options=chrome_options)
 driver.implicitly_wait(10)
 login(driver)
 time.sleep(10)
 errorcnt = 0
 while check(driver) == False:
     errorcnt += 1
-    print('重试中...')
+    print('重试中...', flush=True)
     if errorcnt >= 5:
-        print('错误：无法登录')
+        print('错误：无法登录', flush=True)
         driver.quit()
         exit(1)
     login(driver)
@@ -103,10 +104,10 @@ while punch(driver) == False:
     if get_result(driver) == False:
         errorcnt += 1
         if errorcnt >= 5:
-            print('错误：无法打卡')
+            print('错误：无法打卡', flush=True)
             driver.quit()
             exit(3)
-        print('重试中...')
+        print('重试中...', flush=True)
     else:
         driver.quit()
         exit(0)
